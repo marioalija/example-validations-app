ccf2e8cb424c5b555b046b4294fb61d523a766f9
marioalija <marioalija25@gmail.com>
2023-10-30 13:31:54 -0500

    -m

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     render :index
   end
 
+  def show
+    @user = User.find_by(id: params["id"])
+    render :show
+  end
+
   def update
     @user = User.find_by(id: params[:id])
     @user.update(
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "/users" => "users#index"
   get "/users/:id" => "users#show"
   post "/users" => "users#create"
-  patch "/users/:id" => "users#update"
+  patch "/users/:id " => "users#update"
 end
